48792b0a693ea54b045c1f6e6742b8db68121ee0
jaceguaidemagalhaes <jaceguai@gmail.com>
2022-03-12 23:35:36 -0500

    task 15 Create class Health Data Still miss test read JSON

--- src/main/scala/alvicenna/HelloWord.scala
+++ src/main/scala/alvicenna/HelloWord.scala
@@ -3,4 +3,7 @@ package alvicenna
 class HelloWord {
   println("Hello Word")
 
+  import java.util.Calendar
+  Calendar.getInstance().getTime()
+
 }
--- src/main/scala/alvicenna/Main.scala
+++ src/main/scala/alvicenna/Main.scala
@@ -10,7 +10,8 @@ object Main extends App {
 
     // system configuration properties
     var defaultUser = 1
-    var defaultPatient = 1
+    var defaultPatient = 0
+    var defautlPatientMap:Map[Int,(String,String,String,String,String)] = null
     var activity = ""
     var option = ""
     var exit = false
@@ -33,11 +34,13 @@ object Main extends App {
         println(s"${RESET}${BLUE}${BOLD}Select your option: (Type your option number)${RESET}")
         println()
         println(s"${RESET}${BLUE}1 -> Patient${RESET}")
+        println(s"${RESET}${BLUE}2 -> Health Data${RESET}")
         println(s"${RESET}${BLUE}0 -> Exit APP${RESET}")
         print(s"> ")
         option = readLine().trim()
         option match {
             case "1" => patientScreen()
+            case "2" => selectPatientScreen()
             case "0" => {
                 println("Exit APP")
                 exit = true
@@ -46,6 +49,55 @@ object Main extends App {
             // end match
         }
         //end main screen
+    }
+
+    def selectPatientScreen(): Unit ={
+        topScreen()
+        println()
+        println(s"               ${RESET}${GREEN_B}${BOLD}Select Patient${RESET}")
+        println()
+        var patient = new Patient()
+        patient.selectDefaultPatient()
+        if(defaultPatient != 0) healthDataScreen()
+
+        //end selectPatientScreen
+    }
+
+    def healthDataScreen(): Unit ={
+        var healthData = new HealthData()
+        var localOption = ""
+        var localExit = false
+        topScreen()
+        do {
+            println()
+            println(s"               ${RESET}${GREEN_B}${BOLD}Health Data Screen${RESET}")
+            println()
+            println(s"${RESET}${BOLD}Patient: ${defautlPatientMap(defaultPatient)._1} ${defautlPatientMap(defaultPatient)._2}${RESET}")
+            println()
+            println(s"${RESET}${BLUE}${BOLD}Select your option: (Type your option number)${RESET}")
+            println()
+            println(s"${RESET}${BLUE}1 -> Insert new Health Data${RESET}")
+            println(s"${RESET}${BLUE}2 -> List Health Data${RESET}")
+            println(s"${RESET}${BLUE}3 -> Read JSON file${RESET}")
+            println(s"${RESET}${BLUE}4 -> Delete Health Data${RESET}")
+            println(s"${RESET}${BLUE}5 -> Return to Main Screen${RESET}")
+            println(s"${RESET}${BLUE}0 -> Exit APP${RESET}")
+            print(s"> ")
+            localOption = readLine().trim()
+            localOption match {
+                case "1" => healthData.create()
+                case "2" => healthData.read()
+                case "3" => println("Under Construction")
+                case "4" => healthData.delete()
+                case "5" => localExit = true
+                case "0" => { localExit = true
+                    exit = true}
+                case other => println(s"${RESET}${RED}Wrong option!${RESET}")
+            }
+            // end match
+        }
+        while (!localExit)
+        //end patientScreen
     }
 
     def patientScreen(): Unit ={
